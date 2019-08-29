7c774b4574b3197a5a50f7b4d8331ab1baeafaa0
Jorge Betancur <jabetancur12@gmail.com>
2019-08-28 19:30:01 -0500

    origin maser

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,19 +1,19 @@
-class PinsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+
+Learn more or give us feedback
+class Api::V1::PinsController < ApplicationController
+  before_action :authenticate_api!
 
   def index
-    render json: Pin.all.order('created_at DESC')
+      render json: Pin.all.order('created_at DESC')
   end
 
-
   def create
-    pin = Pin.new(pin_params)
-    pin.user = current_user
-    if pin.save
-      redirect_to root_path, notice: "El pin fue creado exitosamente"
-    else
-      render :new
-    end
+      pin = Pin.new(pin_params)
+      if pin.save
+        render json: pin, status: 201
+      else
+        render json: { errors: pin.errors }, status: 422
+      end
   end
 
   private
