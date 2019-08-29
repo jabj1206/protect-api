9bac42035c3239a80b0b826531759c4f61fd8017
Jorge Betancur <jabetancur12@gmail.com>
2019-08-28 19:27:29 -0500

    aut

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,12 +2,9 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @pins = Pin.all.order('created_at DESC')
+    render json: Pin.all.order('created_at DESC')
   end
 
-  def new
-    @pin = Pin.new
-  end
 
   def create
     pin = Pin.new(pin_params)
@@ -22,5 +19,14 @@ class PinsController < ApplicationController
   private
     def pin_params
       params.require(:pin).permit(:title, :image_url)
+    end
+
+    def authenticate_api!
+      email = request.headers['HTTP_X_USER_EMAIL']
+      token = request.headers['HTTP_X_API_TOKEN']
+      @current_user = User.where(email: email).take
+      unless @current_user && @current_user.api_token == token
+        head 401
+      end
     end
 end
